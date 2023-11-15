8b6f3645c0eb7270f59d5611c490f21afc4a49ba
intimyp <intimyp@gmail.com>
2023-11-15 22:29:29 +0100

    Add sphere

--- deeplearn/__main__.py
+++ deeplearn/__main__.py
@@ -1,1 +1,19 @@
-# Implement a transformer.
+# Use batch normalization.
+
+import numpy
+import math
+import matplotlib.pyplot as pyplot
+
+radians = numpy.linspace(-math.pi, math.pi)
+theta_2d, phi_2d = numpy.meshgrid(radians, radians)
+theta = theta_2d.ravel()
+phi = phi_2d.ravel()
+x = numpy.sin(theta) * numpy.cos(phi)
+y = numpy.sin(theta) * numpy.sin(phi)
+z = numpy.cos(theta)
+
+figure = pyplot.figure()
+axes = figure.add_subplot(projection="3d")
+axes.scatter(x, y, z)
+axes.set_aspect('equal')
+pyplot.show()
