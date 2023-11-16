82db4a2539a3319469bb094fb9bb3ca233456a85
intimyp <intimyp@gmail.com>
2023-11-16 19:01:29 +0100

    plot saved test and train data

--- deeplearn/plot.py
+++ deeplearn/plot.py
@@ -1,5 +1,6 @@
 """For plotting data"""
 
+import matplotlib.pyplot as pyplot
 import plotly.graph_objects as graph_objects  # pyright: ignore[reportMissingTypeStubs]
 import torch
 
@@ -22,3 +23,15 @@ def plot_volume(volume_data: torch.Tensor) -> None:
     )
     f = graph_objects.Figure(data=d)
     f.show()  # pyright: ignore[reportUnknownMemberType]
+
+def plot_test_train_data(filename: str):
+    t = torch.load(filename) # pyright: ignore[reportUnknownMemberType]
+    figure = pyplot.figure() # pyright: ignore[reportUnknownMemberType]
+    ax = figure.add_subplot() # pyright: ignore[reportUnknownMemberType]
+    epochs = t[:,0].numpy()
+    train_loss = t[:,1].numpy()
+    test_loss = t[:,2].numpy()
+    ax.plot(epochs, train_loss, label="train") # pyright: ignore[reportUnknownMemberType]
+    ax.plot(epochs, test_loss, label="test") # pyright: ignore[reportUnknownMemberType]
+    ax.legend() # pyright: ignore[reportUnknownMemberType]
+    pyplot.show() # pyright: ignore[reportUnknownMemberType]
