58654db53d0f2e6b325b487a35784a0eb3471b70
intimyp <intimyp@gmail.com>
2023-11-16 19:03:05 +0100

    test and train on file data

--- deeplearn/__main__.py
+++ deeplearn/__main__.py
@@ -5,21 +5,25 @@ import torch.optim as optim
 import deeplearn.data as data
 import deeplearn.model as model
 import deeplearn.train as train
+import deeplearn.plot as plot
 
 learning_rate = 1e-3
-batch_size = 2
-epochs = 5
-size = 1000
-dataset = data.SphereDataset(size)
-loader = data.Loader(dataset, batch_size)
+batch_size = 10
+epochs = 20
+train_loader = data.Loader(data.FileSphereDataset("train_data"), batch_size)
+test_loader = data.Loader(data.FileSphereDataset("test_data"), batch_size)
 net = model.LinearReluStack()
 loss_function: model.TypedModel = (
     nn.MSELoss()
 )  # pyright: ignore[reportGeneralTypeIssues]
 optimizer = optim.SGD(net.parameters(), lr=learning_rate)
 
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train.loop(loader, net, loss_function, optimizer)
-    train.test_loop(loader, net, loss_function)
-print("Done!")
+train.train_test_loops(
+    epochs,
+    train_loader,
+    test_loader,
+    optimizer,
+    net,
+    loss_function,
+)
+plot.plot_test_train_data("epoch_train_test.data")
