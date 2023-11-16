b930323773285dd8f2d22f862972e5493c1912b9
intimyp <intimyp@gmail.com>
2023-11-16 11:33:58 +0100

    Add isort and strict type checking

--- deeplearn/data.py
+++ deeplearn/data.py
@@ -1,18 +1,24 @@
 """Define dataset"""
 
-import torch.utils.data as data
-import torch
 import dataclasses
+import math
+import typing
+
 import numpy
 import numpy.random as random
-import typing
-import math
+import numpy.typing as ntyping
+import torch
+import torch.utils.data as data
+
+Float3DArray = numpy.ndarray[tuple[int, int, int], numpy.dtype[numpy.float64]]
+
 
 @dataclasses.dataclass
 class Grid:
-    x: numpy.ndarray
-    y: numpy.ndarray
-    z: numpy.ndarray
+    x: Float3DArray
+    y: Float3DArray
+    z: Float3DArray
+
 
 @dataclasses.dataclass
 class Sphere:
@@ -21,36 +27,51 @@ class Sphere:
     z: float
     r: float
 
-    def overlaps(self, other: typing.Self)->bool:
-        distance = math.sqrt((self.x-other.x)**2+(self.y-other.y)**2+(self.z-other.z)**2)
+    def overlaps(self, other: typing.Self) -> bool:
+        distance = math.sqrt(
+            (self.x - other.x) ** 2 + (self.y - other.y) ** 2 + (self.z - other.z) ** 2
+        )
         return distance < (self.r + other.r)
-    
-    def overlaps_any(self, spheres: list[typing.Self])->bool:
+
+    def overlaps_any(self, spheres: list[typing.Self]) -> bool:
         for s in spheres:
             if self.overlaps(s):
                 return True
         return False
-    
-    def to_tensor(self)->torch.Tensor:
+
+    def to_tensor(self) -> torch.Tensor:
         return torch.Tensor([self.x, self.y, self.z, self.r])
 
-GRID = Grid(*numpy.mgrid[-2:2:40j,-2:2:40j,-2:2:40j])
 
-def create_volumetric_sphere(sphere: Sphere)->torch.Tensor:
-    dist_to_center = numpy.sqrt((GRID.x-sphere.x)**2+(GRID.y-sphere.y)**2+(GRID.z-sphere.z)**2)
-    noise = 0.1 * ( random.rand(*GRID.x.shape) - 0.5)
-    return torch.from_numpy(numpy.exp(-100*(dist_to_center-sphere.r+noise)**2))
+GRID = Grid(*numpy.mgrid[-2:2:40j, -2:2:40j, -2:2:40j])
 
-def create_random_sphere(possible_radii: numpy.ndarray, possible_coordinates: numpy.ndarray)->Sphere:
-    coordinates =tuple(random.choice(possible_coordinates,3))
-    radius = random.choice(possible_radii)
-    return Sphere(*coordinates,radius)
 
-class SphereDataset(data.Dataset):
-    def __init__(self, length: int, overlap_allowed = True) -> None:
+def create_volumetric_sphere(sphere: Sphere) -> torch.Tensor:
+    dist_to_center = numpy.sqrt(
+        (GRID.x - sphere.x) ** 2 + (GRID.y - sphere.y) ** 2 + (GRID.z - sphere.z) ** 2
+    )
+    noise = 0.1 * (random.rand(*GRID.x.shape) - 0.5)
+    return torch.from_numpy(  # pyright: ignore[reportUnknownMemberType]
+        numpy.exp(-100 * (dist_to_center - sphere.r + noise) ** 2)
+    )
+
+
+def create_random_sphere(
+    possible_radii: ntyping.NDArray[numpy.float64],
+    possible_coordinates: ntyping.NDArray[numpy.float64],
+) -> Sphere:
+    coordinates: tuple[float, float, float] = tuple(
+        random.choice(possible_coordinates, 3)
+    )  # pyright: ignore[reportGeneralTypeIssues]
+    radius: float = random.choice(possible_radii)
+    return Sphere(*coordinates, float(radius))
+
+
+class SphereDataset(data.Dataset[tuple[torch.Tensor, torch.Tensor]]):
+    def __init__(self, length: int, overlap_allowed: bool = True) -> None:
         super().__init__()
-        radii = numpy.linspace(0.2, 0.5, 5)
-        coordinate = numpy.linspace(-1,1, 5)
+        radii = numpy.linspace(0.2, 0.5, 5, dtype=numpy.float64)
+        coordinate = numpy.linspace(-1, 1, 5, dtype=numpy.float64)
         self._spheres: list[Sphere] = []
         tries = 0
         while len(self._spheres) < length:
@@ -65,7 +86,7 @@ class SphereDataset(data.Dataset):
 
     def __len__(self):
         return len(self._spheres)
-    
+
     def __getitem__(self, index: int) -> tuple[torch.Tensor, torch.Tensor]:
         sphere = self._spheres[index]
         sphere_volume = create_volumetric_sphere(sphere)
--- deeplearn/plot.py
+++ deeplearn/plot.py
@@ -1,10 +1,12 @@
 """For plotting data"""
 
-import plotly.graph_objects as graph_objects
+import plotly.graph_objects as graph_objects  # pyright: ignore[reportMissingTypeStubs]
 import torch
+
 import deeplearn.data as data
 
-def plot_volume(volume_data: torch.Tensor)->None:
+
+def plot_volume(volume_data: torch.Tensor) -> None:
     """Create a volumetric plot of values in grid."""
     volume_data_numpy = volume_data.numpy()
     assert volume_data_numpy.shape == data.GRID.x.shape
@@ -15,8 +17,8 @@ def plot_volume(volume_data: torch.Tensor)->None:
         value=volume_data_numpy.flatten(),
         isomin=volume_data_numpy.min(),
         isomax=volume_data_numpy.max(),
-        opacity=0.1, 
+        opacity=0.1,
         surface_count=20,
     )
     f = graph_objects.Figure(data=d)
-    f.show()
+    f.show()  # pyright: ignore[reportUnknownMemberType]
